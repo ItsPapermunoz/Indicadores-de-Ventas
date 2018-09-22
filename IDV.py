82fc3558da228729d764058ea607ded5d605aa1d
Rodrigo Muñoz Muñoz <rodrigo.mcuadrada@gmail.com>
2018-09-22 13:45:31 -0500

    Alpha (Close To working)

--- IDV.py
+++ IDV.py
@@ -4,7 +4,7 @@ __version__ = "Alpha"
 __author__ = "Rodrigo 'ItsPaper' Muñoz"
 __email__ = "rodrigo.mcuadrada@gmail.com"
 __license__ = "MIT"
-__date__ = "16/09/18"
+__date__ = "22/09/18"
 
 # Module Imports
 import IEM as iem
@@ -14,7 +14,6 @@ import pickle as pkl
 
 # Variable Declarations
 today = time.strftime("%c")
-reports = []
 
 # Class definitions
 
@@ -52,6 +51,7 @@ class Report:
 
 
 def report_log():
+    """Tries to find a previous DataLog.data File, creates a new one if FileNotFound Exception is run"""
     try:
         reports = pkl.load(open("DataLog.data", "rb"))
     except FileNotFoundError:
@@ -62,11 +62,13 @@ def report_log():
 
 
 def save(report_list):
+    """Dumps report list into Datalog.data"""
     pkl.dump(report_list, open("DataLog.data", "wb"))
     print("Succesfully saved to DataLog.data!")
 
 
 def report_entry():
+    """Creates a new report"""
     date = iem.DP()
     net_salesr = iem.UEIP("Net Sales: ", 0)
     net_salesb = iem.UEIP("Budget Net Sales: ", 0)
@@ -79,11 +81,114 @@ def report_entry():
     entry.review()
     reports.append(entry)
     save(reports)
+    print("Report entry successful!")
+    iem.pause()
     return reports
+
+
+def report_del():
+    """Deletes a report entry"""
+    if len(reports) == 0:
+        print("There are no reports!")
+        iem.pause()
+        return
+    else:
+        i = 1
+        for report in reports:
+            date = iem.PD(report.date)
+            print("Report number {}. Date: {}".format(i, date))
+            i += 1
+        while True:
+            try:
+                del_prompt = iem.UEIP("Which Report number do you want to delete? ", 0)
+                del_prompt -= 1
+                reports.remove(reports[del_prompt])
+                save(reports)
+                print("Report successfully deleted!")
+                iem.pause()
+                return reports
+            except IndexError:
+                print("There is no such report...")
+                iem.pause()
+
+
+def report_view():
+    if len(reports) == 0:
+        print("There are no reports to view!")
+        iem.pause()
+        return
+    else:
+        i = 1
+        for report in reports:
+            date = iem.PD(report.date)
+            print("Report number {}. Date: {}".format(i, date))
+            i += 1
+        while True:
+            try:
+                view_prompt = iem.UEIP("Which report do you want to Preview? ", 0)
+                view_prompt -= 1
+                view_prompt = reports[view_prompt]
+                view_prompt.review()
+                break
+            except IndexError:
+                print("There is no such report...")
+                iem.pause()
+
+
+def wipe(report_list):
+    print("Are you sure you want to delete all reports?")
+    if iem.confirm():
+        report_list = []
+        save(report_list)
+        print("Data successfully wiped...")
+        iem.pause()
+        return report_list
+    else:
+        iem.pause()
+        return report_list
+
+
+def menu(reports):
+    """This is the main Menu Loop. that runs all of the functions based on the  user option selection"""
+    while True:
+        iem.cls()
+        print("Welcome to ItsPaper's Sales indicator.")
+        print("Please Select one of the following options...")
+        print("Option 1. View Reports\nOption 2. Enter a new Report\nOption 3. Delete an existing report")
+        print("Option 4. Wipe all data\nOption 5. Credits\nOption 6. Exit Program!")
+        menusel = iem.UEIP("\nPlease enter the number of the option... ", 0)
+        if menusel == 1:
+            iem.cls()
+            report_view()
+        elif menusel == 2:
+            iem.cls()
+            reports = report_entry()
+        elif menusel == 3:
+            iem.cls()
+            reports = report_del()
+        elif menusel == 4:
+            iem.cls()
+            reports = wipe(reports)
+        elif menusel == 5:
+            iem.cls()
+            print("ItsPaper's Sales indicator\n Made by: {}.".format(__author__))
+            print("Last Edited: {} Current Version loaded: {}".format(__date__, __version__))
+            print("Contact me at: {}".format(__email__))
+            iem.pause()
+        elif menusel == 6:
+            iem.cls()
+            print("Thank you for using ItsPaper's Sales Indicator!")
+            iem.pause()
+            break
+        else:
+            print("Option not recognized, please try again!")
+            iem.pause()
 
 
 # Main Code
 
+reports = report_log()
+menu(reports)
 
 
 
